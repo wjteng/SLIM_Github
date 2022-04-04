e6da847ee78124b93ab62fa0c442901d3c185530
weiijian4th <102636929+weiijian4th@users.noreply.github.com>
2022-04-04 18:02:27 +0800

    Update SLIM.py

--- SLIM.py
+++ SLIM.py
@@ -87,26 +87,6 @@ def dec_one_round(c,k):
 
     return(l,r);
 
-def enc_one_round_LBC(p,k):
-    l,r =  p[0], p[1];
-
-    #P2
-    r_p = permute(r,P2);
-   
-    #Left Shift   
-    r = rol(r,7);
-
-    #Substitution
-    r_s = substitute (r,S);
-
-    #Modular Addition
-    l= (l+r_s+k) & MASK_VAL;
-
-    #P1
-    r = permute(l,P1);
-
-    l=r_p;
-    return(l,r);
 
 #def dec_one_round(c,k):
 #    c0, c1 = c[0], c[1];
@@ -172,56 +152,6 @@ def expand_key(k,t):
          
     return(ks);
 
-def expand_key_LBC(k,t):
-    ks = [0 for i in range(t)];
-    
-    L = (2**32)*np.uint64(k[2]&0xff)+(2**16)*np.uint64(k[3])+np.uint64(k[4]);
-    M = (2**24)*np.uint64(k[0])+(2**8)*np.uint64(k[1])+np.uint64(k[2]>>8);
-     
-    for i in range(t):
-        if i<5:
-          ks[i]=k[4-i];
-        else:
-          Lnib = np.zeros((4,int(k.size/5)),dtype=np.int16);
-          Mnib = np.zeros((4,int(k.size/5)),dtype=np.int16);
-
-          Lnib[0] = np.uint16(L&0xf);
-          Lnib[1] = np.uint16((L>>4)&0xf);
-          Lnib[2] = np.uint16((L>>8)&0xf);
-          Lnib[3] = np.uint16((L>>12)&0xf);
-
-          Mnib[0] = np.uint16((M)&0xf);
-          Mnib[1] = np.uint16((M>>4)&0xf);
-          Mnib[2] = np.uint16((M>>8)&0xf);
-          Mnib[3] = np.uint16((M>>12)&0xf);
-
-          Lnib = rolnib(Lnib,3);
-
-          Lnib = Lnib^Mnib;
-
-          for i in range(0,Lnib.shape[0]):
-            for j in range(0,Lnib.shape[1]):
-              Lnib[i][j]=S1[Lnib[i][j]];
-
-          M_16 = Mnib[0] + Mnib[1]*(2**4) + Mnib[2]*(2**8) + np.uint16(Mnib[3])*(2**12);
-          M_16 = permute(M_16,P1);
-
-          Mnib[0] = np.uint16((M_16)&0xf);
-          Mnib[1] = np.uint16((M_16>>4)&0xf);
-          Mnib[2] = np.uint16((M_16>>8)&0xf);
-          Mnib[3] = np.uint16((M_16>>12)&0xf);
-
-          Mnib = Mnib^Lnib;
-
-          L_16 = Lnib[0] + Lnib[1]*(2**4) + Lnib[2]*(2**8) + np.uint16(Lnib[3])*(2**12);
-          M_16 = Mnib[0] + Mnib[1]*(2**4) + Mnib[2]*(2**8) + np.uint16(Mnib[3])*(2**12);
-
-          M = np.uint64(M>>16) + np.uint64(M_16)*(2**24);
-          L = np.uint64(L>>16) + np.uint64(L_16)*(2**24);
-
-          ks[i] = M_16;
-    return(ks);
-
 def substitute (x , s):
     y = x*0;
     for i in range(0,x.size):
@@ -235,7 +165,6 @@ def substitute (x , s):
 
     return y;
 
-
 def permute(x,p):
     y = x*0;
     for i in range(0,16):
@@ -246,25 +175,16 @@ def permute(x,p):
 
 def encrypt(p, ks):
     x, y = p[0], p[1];
-    i = 1;
     for k in ks:
-
-        x,y = enc_one_round((x,y), k);
-        
-        i=i+1;
-
+      x,y = enc_one_round((x,y), k);
     return(x, y);
 
 def decrypt(c, ks):
     x, y = c[0], c[1];
-    i = 1;
     for k in reversed(ks):
-
-        x,y = dec_one_round((x,y), k);
-        
-        i=i+1;
-
-    return(x, y);
+      x, y = dec_one_round((x,y), k);
+      #x, y = enc_one_round((x,y), k);
+    return(x,y);
 
 #Original
 #def decrypt(c, ks):
@@ -323,6 +243,7 @@ def readcsv(datei):
 
 #baseline training data generator
 def make_train_data(n, nr, diff=(0x0040,0)):
+  print(diff);
   Y = np.frombuffer(urandom(n), dtype=np.uint8); Y = Y & 1;
   keys = np.frombuffer(urandom(10*n),dtype=np.uint16).reshape(5,-1);
   plain0l = np.frombuffer(urandom(2*n),dtype=np.uint16);
