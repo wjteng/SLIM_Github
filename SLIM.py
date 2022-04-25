6ca8e5d34154accb237d1696f3e8628087324fc6
wjteng <33016809+wjteng@users.noreply.github.com>
2022-04-25 14:06:25 +0800

    Update SLIM.py

--- SLIM.py
+++ SLIM.py
@@ -185,8 +185,8 @@ def encrypt(p, ks):
     x, y = p[0], p[1];
   
     for k in ks:
-      x,y = enc_one_round((x,y), 0);
-      #x,y = enc_one_round((x,y), k);
+      #x,y = enc_one_round((x,y), 0);
+      x,y = enc_one_round((x,y), k);
     return(x, y);
 
 def decrypt(c, ks):
