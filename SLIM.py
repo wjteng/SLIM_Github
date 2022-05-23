2aa603510ea4cbef675e52e0e76bb3dbfdfc167f
wjteng <33016809+wjteng@users.noreply.github.com>
2022-05-23 14:15:58 +0800

    Update SLIM.py

--- SLIM.py
+++ SLIM.py
@@ -53,20 +53,20 @@ def enc_one_round(p,k):
     l,r =  p[0], p[1];
 
     #original#
-    #r_k = (r ^ k) ;
+    #r_1 = r*1;
+    #r_k = (r_1 ^ k) ;
     #r_s = substitute(r_k,S);
     #r_p = permute(r_s, P);    
     
-    r_1 = r * 1;
-    r_k = (r_1 ^ k) ;
-    r_p = permute(r_k, P); 
-    r_s = substitute(r_p,S);
-    
+    r_1 = r*1;
+    r_s = substitute(r_1,S);
+    r_p = permute(r_s, P); 
 
     l_temp = l*1;
-    l_temp = (l_temp^r_s) ;
+    l_temp = (l_temp^r_p) ;
 
     #l = r_k;
+    r_k = (r_1 ^ k) ;
     l = r_k;
     r = l_temp;
     
